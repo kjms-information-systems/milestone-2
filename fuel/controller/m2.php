3528899d083d90db19c878b48fcc74020db3f794
Marvin Noe Enriquez <enriquezmarvin49@gmail.com>
2019-03-06 10:57:45 -0700

    Create m2.php

---
+++ fuel/controller/m2.php
@@ -0,0 +1,42 @@
+<?php
+
+use \Model\Form;
+
+class Controller_M2 extends Controller_Template {
+
+#pages belong her and live inside of the view folder
+
+	public function action_index() {
+		$data = array();
+		$this->template->title = 'KJMS';
+		$this->template->subtitle = 'KJMS';
+		$this->template->content = View::forge('m2/index', $data);
+	}
+
+	public function action_about() {
+        $data = array();
+		$this->template->title = 'KJMS';
+		$this->template->subtitle = 'About';
+		$this->template->content = View::forge('m2/about', $data);
+
+	}
+	
+    public function action_moreinfo() {
+        $data = array();
+		$this->template->title = 'KJMS';
+		$this->template->subtitle = 'More Information';
+		$this->template->content = View::forge('m2/moreinformation', $data);
+
+	}
+
+	public function action_bg() {
+
+	}
+
+// FORM EXAMPLE -------------------------------------------
+	public function action_list() {
+
+	}
+// --------------------------------------------------------
+
+}
