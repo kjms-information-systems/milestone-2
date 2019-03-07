085750bcc020ca49c10de284177fca836acb7287
kmlindgren10 <34612438+kmlindgren10@users.noreply.github.com>
2019-03-07 11:25:51 -0700

    Added all safety

--- fuel/view/m2/vbp_modeling.php
+++ fuel/view/m2/vbp_modeling.php
@@ -35,74 +35,74 @@ echo Form::open(array('action' => 'index/m2/vbp_modeling', 'method' => 'post'));
 								<td><?php echo $psi90[6];?> out of 10</td>
 							</tr>
 							<tr>
-								<td id="category">HA1</td>
+								<td id="category">HAI-1</td>
 								<td>0.369</td>
 								<td>0</td>
-								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
-								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
-								<td><?php echo $answer[2];?> out of 10</td>
-								<td><?php echo $answer[2];?> out of 9</td>
-								<td><?php echo $answer[2];?> out of 10</td>
+								<td><?php echo Form::input('b_ha1', $ha1[2], array('class' => 'form-control'));?></td>
+								<td><?php echo Form::input('p_ha1', $ha1[3], array('class' => 'form-control'));?></td>
+								<td><?php echo $ha1[4];?> out of 10</td>
+								<td><?php echo $ha1[5];?> out of 9</td>
+								<td><?php echo $ha1[6];?> out of 10</td>
 							</tr>
 							<tr>
 								<td id="category">HAI-2</td>
 								<td>0.906</td>
 								<td>0</td>
-								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
-								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
-								<td><?php echo $answer[2];?> out of 10</td>
-								<td><?php echo $answer[2];?> out of 9</td>
-								<td><?php echo $answer[2];?> out of 10</td>
+								<td><?php echo Form::input('b_ha2', $ha2[2], array('class' => 'form-control'));?></td>
+								<td><?php echo Form::input('p_ha2', $ha2[3], array('class' => 'form-control'));?></td>
+								<td><?php echo $ha2[4];?> out of 10</td>
+								<td><?php echo $ha2[5];?> out of 9</td>
+								<td><?php echo $ha2[6];?> out of 10</td>
 							</tr>
 							<tr>
 								<td id="category">HAI-3</td>
 								<td>0.824</td>
 								<td>0</td>
-								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
-								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
-								<td><?php echo $answer[2];?> out of 10</td>
-								<td><?php echo $answer[2];?> out of 9</td>
-								<td><?php echo $answer[2];?> out of 10</td>
+								<td><?php echo Form::input('b_ha3', $ha3[2], array('class' => 'form-control'));?></td>
+								<td><?php echo Form::input('p_ha3', $ha3[3], array('class' => 'form-control'));?></td>
+								<td><?php echo $ha3[4];?> out of 10</td>
+								<td><?php echo $ha3[5];?> out of 9</td>
+								<td><?php echo $ha3[6];?> out of 10</td>
 							</tr>
 							<tr>
 								<td id="category">HAI-4</td>
 								<td>0.71</td>
 								<td>0</td>
-								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
-								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
-								<td><?php echo $answer[2];?> out of 10</td>
-								<td><?php echo $answer[2];?> out of 9</td>
-								<td><?php echo $answer[2];?> out of 10</td>
+								<td><?php echo Form::input('b_ha4', $ha4[2], array('class' => 'form-control'));?></td>
+								<td><?php echo Form::input('p_ha4', $ha4[3], array('class' => 'form-control'));?></td>
+								<td><?php echo $ha4[4];?> out of 10</td>
+								<td><?php echo $ha4[5];?> out of 9</td>
+								<td><?php echo $ha4[6];?> out of 10</td>
 							</tr>
 							<tr>
 								<td id="category">HAI-5</td>
 								<td>0.767</td>
 								<td>0</td>
-								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
-								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
-								<td><?php echo $answer[2];?> out of 10</td>
-								<td><?php echo $answer[2];?> out of 9</td>
-								<td><?php echo $answer[2];?> out of 10</td>
+								<td><?php echo Form::input('b_ha5', $ha5[2], array('class' => 'form-control'));?></td>
+								<td><?php echo Form::input('p_ha5', $ha5[3], array('class' => 'form-control'));?></td>
+								<td><?php echo $ha5[4];?> out of 10</td>
+								<td><?php echo $ha5[5];?> out of 9</td>
+								<td><?php echo $ha5[6];?> out of 10</td>
 							</tr>
 							<tr>
 								<td id="category">HAI-6</td>
 								<td>0.805</td>
 								<td>0.004</td>
-								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
-								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
-								<td><?php echo $answer[2];?> out of 10</td>
-								<td><?php echo $answer[2];?> out of 9</td>
-								<td><?php echo $answer[2];?> out of 10</td>
+								<td><?php echo Form::input('b_ha6', $ha6[2], array('class' => 'form-control'));?></td>
+								<td><?php echo Form::input('p_ha6', $ha6[3], array('class' => 'form-control'));?></td>
+								<td><?php echo $ha6[4];?> out of 10</td>
+								<td><?php echo $ha6[5];?> out of 9</td>
+								<td><?php echo $ha6[6];?> out of 10</td>
 							</tr>
 							<tr>
 								<td id="category">PC-01</td>
 								<td>0.020408</td>
 								<td>0</td>
-								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
-								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
-								<td><?php echo $answer[2];?> out of 10</td>
-								<td><?php echo $answer[2];?> out of 9</td>
-								<td><?php echo $answer[2];?> out of 10</td>
+								<td><?php echo Form::input('b_pc01', $pc01[2], array('class' => 'form-control'));?></td>
+								<td><?php echo Form::input('p_pc01', $pc01[3], array('class' => 'form-control'));?></td>
+								<td><?php echo $pc01[4];?> out of 10</td>
+								<td><?php echo $pc01[5];?> out of 9</td>
+								<td><?php echo $pc01[6];?> out of 10</td>
 							</tr>
 							</table>
 							<br>
@@ -116,13 +116,8 @@ echo Form::open(array('action' => 'index/m2/vbp_modeling', 'method' => 'post'));
     
     <?php
     
-	echo Form::label('Performance score:', 'performance') . ' ';
-	echo Form::input('performance', $answer[0], array('class' => 'form-control'));
-
-	echo Form::label('Achievement:', 'achievement') . ' ';
-	echo Form::input('achievement', $answer[1], array('class' => 'form-control'));
-	echo 'Calculation: ';
-    echo $answer[2];
+	
+	
 	echo '<br><br>';
 
 	echo Form::button('frmbutton', 'Calculate', array('class' => 'btn btn-default'));
