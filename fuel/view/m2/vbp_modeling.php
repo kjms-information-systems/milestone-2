269d5a72bafc2a4fa92721b49bf9cbe482c66fa6
kmlindgren10 <34612438+kmlindgren10@users.noreply.github.com>
2019-03-07 10:08:58 -0700

    Adding in safety table with input functionality

--- fuel/view/m2/vbp_modeling.php
+++ fuel/view/m2/vbp_modeling.php
@@ -1,8 +1,120 @@
+<!DOCTYPE html>
+<html lang='en'>
+
 <?php
 
 //View for VBP modeling. Controls what the page looks like
 
 echo Form::open(array('action' => 'index/m2/vbp_modeling', 'method' => 'post'));
+
+    ?>
+    
+    <h2>Safety Domain Calculations</h2>
+    
+    <table border="1">
+							<tr>
+								<td id="measure"></td>
+								<td id="measure">Achievement Threshold</td>
+								<td id="measure">Benchmark</td>
+								<td id="measure">Baseline Rate</td>
+								<td id="measure">Performance Rate</td>
+								<td id="measure">Achievement Points</td>
+								<td id="measure">Improvement Points</td>
+								<td id="measure">Dimension Score</td>
+								
+							</tr>
+						
+							<tr>
+								<td id="category">PSI-90</td>
+								<td>0.964542</td>
+								<td>0.709498</td>
+								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
+								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
+								<td>10 out of 10</td>
+								<td>9 out of 9</td>
+								<td>10 out of 10</td>
+							</tr>
+							<tr>
+								<td id="category">HA1</td>
+								<td>0.369</td>
+								<td>0</td>
+								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
+								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
+								<td>0 out of 10</td>
+								<td>0 out of 9</td>
+								<td>0 out of 10</td>
+							</tr>
+							<tr>
+								<td id="category">HAI-2</td>
+								<td>0.906</td>
+								<td>0</td>
+								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
+								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
+								<td>3 out of 10</td>
+								<td>0 out of 9</td>
+								<td>2 out of 10</td>
+							</tr>
+							<tr>
+								<td id="category">HAI-3</td>
+								<td>0.824</td>
+								<td>0</td>
+								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
+								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
+								<td>0 out of 10</td>
+								<td>0 out of 9</td>
+								<td>0 out of 10</td>
+							</tr>
+							<tr>
+								<td id="category">HAI-4</td>
+								<td>0.71</td>
+								<td>0</td>
+								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
+								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
+								<td>10 out of 10</td>
+								<td>9 out of 9</td>
+								<td>10 out of 10</td>
+							</tr>
+							<tr>
+								<td id="category">HAI-5</td>
+								<td>0.767</td>
+								<td>0</td>
+								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
+								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
+								<td>0 out of 10</td>
+								<td>0 out of 9</td>
+								<td>0 out of 10</td>
+							</tr>
+							<tr>
+								<td id="category">HAI-6</td>
+								<td>0.805</td>
+								<td>0.004</td>
+								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
+								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
+								<td>0 out of 10</td>
+								<td>4 out of 9</td>
+								<td>4 out of 10</td>
+							</tr>
+							<tr>
+								<td id="category">PC-01</td>
+								<td>0.020408</td>
+								<td>0</td>
+								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
+								<td><?php echo Form::input('performance', $answer[0], array('class' => 'form-control'));?></td>
+								<td>2 out of 10</td>
+								<td>0 out of 9</td>
+								<td>2 out of 10</td>
+							</tr>
+							</table>
+							<br>
+							<table border="1">
+							<tr>
+								<td id="category">Combined SSI Score</td>
+								<td>2 out of 10</td>
+							</tr>
+						</table>
+						
+    
+    <?php
     
 	echo Form::label('Performance score:', 'performance') . ' ';
 	echo Form::input('performance', $answer[0], array('class' => 'form-control'));
