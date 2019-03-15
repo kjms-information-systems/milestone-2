66d5b42a5d14c5e1bdb1615eeecca6ce0ef00e1b
ScottSparks <47544220+ScottSparks@users.noreply.github.com>
2019-03-15 12:25:40 -0600

    Update about.php

--- fuel/view/m2/about.php
+++ fuel/view/m2/about.php
@@ -16,9 +16,9 @@
     <div class="card">
     <?php echo Asset::img('josette.jpg',  array('class' => 'cardPhoto')); ?>
     <br>
-    <h2>Josette Grinslade</h2>
-    <p class="bodyCard">Josette is a third year Computer Science major with a concentration in Human centered at CSU.<br>
-        She loves cool plants, her cat (Louis XVI) and rainy days. She hopes to go to Grad School for Security & Ethics.</p>
+    <h2>Scott Sparks</h2>
+    <p class="bodyCard">Scott is a second year at CSU who is also pursueing a concentration in Human Centered Computing.
+        He likes to play guitar and read in his free time.</p>
     <p>Colorado State University</p>
     </div>
     
