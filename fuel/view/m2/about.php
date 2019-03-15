c167e303d5c432eb1cce76aa86b6413fdcf92610
Marvin Noe Enriquez <enriquezmarvin49@gmail.com>
2019-03-15 12:18:34 -0600

    Update about.php

--- fuel/view/m2/about.php
+++ fuel/view/m2/about.php
@@ -1,12 +1,46 @@
-<!DOCTYPE html>
-<html>
-<h2>Meet the Team</h2>
-<div id="person">
-<h3>Josette Grinslade</h2>
- <div id=image>
-		<?php echo Asset::img('ghc18.jpg', array('id' => 'profpic')); ?>
-	</div>
-<p>Josette is a third year Computer Science major with a concentration in Human centered at CSU.<br>
-She loves cool plants, her cat (Louis XVI) and rainy days. She hopes to go to Grad School for Security & Ethics.</p>
-</div> 
-</html>
+
+
+<div class=cardHolder>
+
+    <div class="card">
+    <?php echo Asset::img('josette.jpg',  array('class' => 'cardPhoto')); ?>
+    <br>
+    
+    <h2>Josette Grinslade</h2>
+    <p class="bodyCard">Josette is a third year Computer Science major with a concentration in Human centered at CSU.<br>
+        She loves cool plants, her cat (Louis XVI) and rainy days. She hopes to go to Grad School for Security & Ethics.</p>
+    <p>Colorado State University</p>
+    </div>
+    
+    
+    <div class="card">
+    <?php echo Asset::img('josette.jpg',  array('class' => 'cardPhoto')); ?>
+    <br>
+    <h2>Josette Grinslade</h2>
+    <p class="bodyCard">Josette is a third year Computer Science major with a concentration in Human centered at CSU.<br>
+        She loves cool plants, her cat (Louis XVI) and rainy days. She hopes to go to Grad School for Security & Ethics.</p>
+    <p>Colorado State University</p>
+    </div>
+    
+
+    <div class="card">
+    <?php echo Asset::img('josette.jpg',  array('class' => 'cardPhoto')); ?>
+    <br>
+    <h2>Josette Grinslade</h2>
+    <p class="bodyCard">Josette is a third year Computer Science major with a concentration in Human centered at CSU.<br>
+        She loves cool plants, her cat (Louis XVI) and rainy days. She hopes to go to Grad School for Security & Ethics.</p>
+    <p>Colorado State University</p>
+    </div>
+    
+    
+    <div class="card">
+    <?php echo Asset::img('josette.jpg',  array('class' => 'cardPhoto')); ?>
+    <br>
+    <h2>Josette Grinslade</h2>
+    <p class="bodyCard">Josette is a third year Computer Science major with a concentration in Human centered at CSU.<br>
+        She loves cool plants, her cat (Louis XVI) and rainy days. She hopes to go to Grad School for Security & Ethics.</p>
+    <p>Colorado State University</p>
+    </div>
+
+
+</div>
