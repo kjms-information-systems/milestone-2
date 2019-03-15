c8752d903948e198e80c9a992fafe3ff0887cc91
ScottSparks <47544220+ScottSparks@users.noreply.github.com>
2019-03-15 12:55:06 -0600

    Update index.php

--- fuel/view/m2/index.php
+++ fuel/view/m2/index.php
@@ -1,4 +1,10 @@
 <!DOCTYPE html>
 <html>
-Index
+<p>This is the website for KJMS Information Systems. We are a student lead company that provides information solutions for real world 
+problems.</p>
+<p>On our cutting edge website, you are able to model hospital vbp scores in real time, changing any of the measures and viewing the results.
+You will also be able to save and load model sets, along with adding comments, so that you can be as productive as possible. Go to VBP Modeling
+to try out our interactive modeling solution.</p>
+<p>If questions arrise, feel free to contact us on our Info page. Want to know who we are? Learn more on our About Us page.</p>
+<br><br>
 </html>
