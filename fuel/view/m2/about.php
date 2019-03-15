5329fc27854b1830f611d45f4cdb16789117d4b1
jhgrins <jhgrinslade@hotmail.com>
2019-03-14 18:26:03 -0600

    Update about.php

--- fuel/view/m2/about.php
+++ fuel/view/m2/about.php
@@ -3,6 +3,9 @@
 <h2>Meet the Team</h2>
 <div id="person">
 <h3>Josette Grinslade</h2>
+ <div id=image>
+		<?php echo Asset::img('ghc18.jpg', array('id' => 'profpic')); ?>
+	</div>
 <p>Josette is a third year Computer Science major with a concentration in Human centered at CSU.<br>
 She loves cool plants, her cat (Louis XVI) and rainy days. She hopes to go to Grad School for Security & Ethics.</p>
 </div> 
