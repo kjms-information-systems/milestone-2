3923b45e0d5a44deb63813068f99ff18e3088d8f
Marvin Noe Enriquez <enriquezmarvin49@gmail.com>
2019-03-15 20:00:21 -0600

    Update index.php

--- fuel/view/m2/index.php
+++ fuel/view/m2/index.php
@@ -1,10 +1,12 @@
-<!DOCTYPE html>
-<html>
-<p>Welcome to the online headquarters for KJMS Information Systems. We are a student lead company that provides information solutions for real world 
-problems.</p>
-<p>On our cutting edge website, you are able to model hospital vbp scores in real time, changing any of the measures and viewing the results.
-You will also be able to save and load model sets, along with adding comments, so that you can be as productive as possible. Go to VBP Modeling
-to try out our interactive modeling solution.</p>
-<p>If questions arrise, feel free to contact us on our Info page. Want to know who we are? Learn more on our About Us page.</p>
-<br><br>
-</html>
+<div class="content">
+    <?php echo Asset::img('KJMSInformationSystems.png',  array('class' => 'homeIMG')); ?>
+    <div class"homePara">
+        <p>Welcome to the online headquarters for KJMS Information Systems. We are a student lead company that provides information solutions for real world 
+        problems.</p>
+        <p>On our cutting edge website, you are able to model hospital vbp scores in real time, changing any of the measures and viewing the results.
+        You will also be able to save and load model sets, along with adding comments, so that you can be as productive as possible. Go to VBP Modeling
+        to try out our interactive modeling solution.</p>
+        <p>If questions arrise, feel free to contact us on our Info page. Want to know who we are? Learn more on our About Us page.</p>
+        <br><br>
+    </div>
+</div>
