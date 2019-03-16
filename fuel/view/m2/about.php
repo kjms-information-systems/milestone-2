fd7807666ff7768b1a45ed6eb3552e1c9d8db581
Marvin Noe Enriquez <enriquezmarvin49@gmail.com>
2019-03-15 19:50:13 -0600

    Update about.php

--- fuel/view/m2/about.php
+++ fuel/view/m2/about.php
@@ -6,7 +6,7 @@
     <?php echo Asset::img('josette.jpg',  array('class' => 'cardPhoto')); ?>
     <br>
     
-    <h2>Josette Grinslade</h2>
+    <h2 class="cardh2" >Josette Grinslade</h2>
     <p class="bodyCard">Josette is a third year Computer Science major with a concentration in Human centered at CSU.<br>
         She loves cool plants, her cat (Louis XVI) and rainy days. She hopes to go to Grad School for Security & Ethics.</p>
     <p>Colorado State University</p>
@@ -14,31 +14,31 @@
     
     
     <div class="card">
-    <?php echo Asset::img('head shot.PNG',  array('class' => 'cardPhoto')); ?>
+    <?php echo Asset::img('kiera-profile.jpeg',  array('class' => 'cardPhoto')); ?>
     <br>
-    <h2>Scott Sparks</h2>
-    <p class="bodyCard">Scott is a second year at CSU who is also pursueing a concentration in Human Centered Computing.
-        He likes to play guitar and read in his free time.</p>
-    <p>Colorado State University</p>
+    <h2 class="cardh2">Kiera Lindgren</h2>
+    <p class="bodyCard">Kiera is a fourth year Computer Science major with a concentration in Human Centered Computing at CSU. <br>
+        She enjoys playing ultimate frisbee in her free time and is graduating in May. </p>
+         <p>Colorado State University</p>
     </div>
     
 
     <div class="card">
-    <?php echo Asset::img('josette.jpg',  array('class' => 'cardPhoto')); ?>
+    <?php echo Asset::img('scott.png',  array('class' => 'cardPhoto')); ?>
     <br>
-    <h2>Josette Grinslade</h2>
-    <p class="bodyCard">Josette is a third year Computer Science major with a concentration in Human centered at CSU.<br>
-        She loves cool plants, her cat (Louis XVI) and rainy days. She hopes to go to Grad School for Security & Ethics.</p>
+    <h2 class="cardh2">Scott Sparks</h2>
+     <p class="bodyCard">Scott is a second year at CSU who is also pursueing a concentration in Human Centered Computing.
+    <br>He likes to play guitar and read in his free time.</p>
     <p>Colorado State University</p>
     </div>
     
     
     <div class="card">
-    <?php echo Asset::img('josette.jpg',  array('class' => 'cardPhoto')); ?>
+    <?php echo Asset::img('marvin.jpg',  array('class' => 'cardPhoto')); ?>
     <br>
-    <h2>Josette Grinslade</h2>
-    <p class="bodyCard">Josette is a third year Computer Science major with a concentration in Human centered at CSU.<br>
-        She loves cool plants, her cat (Louis XVI) and rainy days. She hopes to go to Grad School for Security & Ethics.</p>
+    <h2 class="cardh2">Marvin Enriquez</h2>
+    <p class="bodyCard">I am Marvin Enriquez a senior studying Applied Computing Technology. When I'm not working on programming I like to snowboard and stay active.
+    <br>I also really enjoy fashion and new trends in pop culture.</p>
     <p>Colorado State University</p>
     </div>
 
